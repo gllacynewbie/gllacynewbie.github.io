72c124d23647594d8cdd4e604391681a5e00dad3
gllacynewbie <creak531@yandex.ru>
2017-06-15 12:42:44 +0300

    first commit

---
+++ js/common.js
@@ -0,0 +1,20 @@
+$('.forDrop').click (function () {
+    		if($('.dropdown').is(':visible'))
+    		   $('.dropdown').hide ();
+    		else 
+    			$('.dropdown').show ();
+});
+
+$('.searchDrop').click (function () {
+    		if($('.hiddenSearch').is(':visible'))
+    		   $('.hiddenSearch').hide ();
+    		else 
+    			$('.hiddenSearch').show ();
+});
+
+$('.entry').click (function () {
+    		if($('.loginForm').is(':visible'))
+    		   $('.loginForm').hide ();
+    		else 
+    			$('.loginForm').show ();
+});
